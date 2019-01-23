13fa0450b9da9d3d34580c66955d00360c464720
kallappa <kallappabachagundi@gmail.com>
2019-01-24 01:27:15 +0530

    final code

--- test.cpp
+++ test.cpp
@@ -12,7 +12,7 @@ template <class type>
 class Stack
 {
 	type s[MAX];
-public :
+	public :
 	Stack()
 	{
 		int i=0;
@@ -24,49 +24,115 @@ public :
 template <class type>
 void Stack<type>::ICFD(int &i)
 {
+	system("clear");
 	int op;
-	//int i=0;
+	char rep;
+	int chance=0,z=0;
+	if(i != 0)
+	{
+		neat(); cout << "Previously you quit a pushing of data in middle of the process. " ;
+		neat(); cout << "Do you want continue ..... : Y/N ";
+take:
+		neat();  cin >> rep;
+		if(rep == 'N' || rep == 'n')
+			i=0;
+		else if(rep == 'y' || rep == 'Y')
+			i = i;
+		else
+		{
+			neat(); cout << "Please enter correct option .... ";
+			if(z < 2)
+				goto take;
+			else
+				neat(); cout << "SORRY YOU CROSSED MAXIMUM LIMITS ";
+			return;
+		}
+	}
 above :
+	//neat();	cout << "------------------------------------------------------------------------";	
 	neat();	cout << "------------------------------ Process Menu ----------------------------";	
+	neat();	cout << "------------------------------------------------------------------------";	
 	neat();	cout << " 1.Push";	
 	neat();	cout << " 2.Pop ";
 	neat();	cout << " 3.Display";
-	neat(); cout << " 4.Exit   " << endl;
+	neat(); cout << " 4.Goto Main menu" << endl;
 	neat(); cout << " --> ";
-		
-		cin  >> op;
+
+	cin  >> op;
 	if(op == 1)
 	{
-		if(i== MAX){
-		neat();	cout << "************************************************************************";
-		neat();	cout << "*************************** STACK OVERFLOW *****************************";
-		neat();	cout << "************************************************************************";
-		goto above;}
-	neat(); cin >> s[i];
-	i++;
-	neat(); cout << "Data is stored ";
-	goto above;
+		chance = 0;
+		if(i== MAX)
+		{
+			system("clear");
+			neat();	cout << "************************************************************************";
+			neat();	cout << "*************************** STACK OVERFLOW *****************************";
+			neat();	cout << "************************************************************************";
+			neat(); 
+			neat(); cout << "Do you want continue ..... : Y/N ";
+			neat();  cin >> rep;
+			if(rep == 'y' || rep == 'Y')
+				goto above;
+			else
+				return;
+		}
+		neat(); cin >> s[i];
+		i++;
+		neat(); cout << "Data is stored ";
+		goto above;
 	}
 	else if(op == 2)
 	{
-	--i;
-		if(i == 0){
-		neat();	cout << "************************** STACK UNDERFLOW *****************************";
-		goto above;}
-	s[i] = 0;
-	neat(); cout << "Data is Deleted " ;
-	goto above;
+		chance = 0;
+		if(i == 0)
+		{
+			system("clear");
+			neat();	cout << "************************************************************************";
+			neat();	cout << "************************** STACK UNDERFLOW *****************************";
+			neat();	cout << "************************************************************************";
+			neat(); 
+			neat(); cout << "Do you want continue ..... : Y/N ";
+			neat();  cin >> rep;
+			if(rep == 'y' || rep == 'Y')
+				goto above;
+			else
+				return;
+			//-------------------------------------------------------------------
+		}
+		--i;
+		neat(); cout << s[i] <<" - is Deleted " ;
+		s[i] = 0;
+		goto above;
 	}
 	else if(op == 3)
 	{
-	neat(); cout << "Elements of Stack : ";
-	neat();
-	for(int k=0;k<MAX;k++)
-		cout << s[k] << "  "; 
-	goto above;
+		chance = 0;
+		neat(); cout << "Elements of Stack : ";
+		neat();
+		for(int k=0;k<MAX;k++)
+			cout << s[k] << "  "; 
+		goto above;
 	}
-	else 
-	return ;	
+	else if(op == 4)
+	{	
+	terminate:
+		return;
+	}
+	else
+	{
+		if(chance < 2)
+		{
+			system("clear");//neat();
+			neat(); cout << "WRONG OPTION : " << 2-chance << " chances remaining." << endl;
+			chance +=1;
+			goto above;
+		}
+		else 
+		{
+			neat(); cout << "SORRY YOU HAVE MADE MAXIMUM ATTEMPTS " << endl;
+			goto terminate;
+		}
+	}	
 
 }
 int main()
@@ -75,9 +141,11 @@ int main()
 	Stack <char>obj2;static int j;
 	Stack <float>obj3;static int k;
 	Stack <double>obj4;static int l;
-	int exit,chance=0;
-repeat :
+	int exit;
+	int chance=0;
+repeat1:
 	system("clear");
+repeat :
 	neat();	cout << "************************************************************************";	
 	neat();	cout << "                           STACK IMPLIMENTATION                         "; 
 	neat();	cout << "************************************************************************";	
@@ -88,60 +156,55 @@ repeat :
 	neat();	cout << " 4.Double ";
 	neat(); cout << " 5.Exit   " << endl;
 	neat(); cout << " --> ";
-		
-		cin  >> exit;
+
+	cin  >> exit;
 	if(exit == 1)
 	{
-	chance=0;
-	neat(); cout << "---------------- STACK APPLICATION IS READY FOR INTEGER ----------------";
-	obj1.ICFD(i);
-	goto repeat;
+		chance=0;
+		neat(); cout << "---------------- STACK APPLICATION IS READY FOR INTEGER ----------------";
+		obj1.ICFD(i);
+		goto repeat1;
 	}
 	else if(exit == 2)
 	{
-	chance=0;
-	neat(); cout << "---------------- STACK APPLICATION IS READY FOR CHARCTER ---------------";
-	obj2.ICFD(j);
-	goto repeat;
+		chance=0;
+		neat(); cout << "---------------- STACK APPLICATION IS READY FOR CHARCTER ---------------";
+		obj2.ICFD(j);
+		goto repeat1;
 	}
 	else if(exit == 3)
 	{
-	chance=0;
-	neat(); cout << "---------------- STACK APPLICATION IS READY FOR FLOAT   ----------------";
-	obj3.ICFD();
-	goto repeat;
+		chance=0;
+		neat(); cout << "---------------- STACK APPLICATION IS READY FOR FLOAT   ----------------";
+		obj3.ICFD(k);
+		goto repeat1;
 	}
 	else if(exit == 4)
 	{	
-	chance=0;
-	neat(); cout << "---------------- STACK APPLICATION IS READY FOR DOUBLE  ----------------";
-	obj4.ICFD();
-	goto repeat;
+		chance=0;
+		neat(); cout << "---------------- STACK APPLICATION IS READY FOR DOUBLE  ----------------";
+		obj4.ICFD(l);
+		goto repeat1;
 	}
 	else if(exit == 5)
 	{
-	neat(); cout << "THANKS FOR BEING HERE " ;
-	neat(); cout << "PROCESS TERMINATING.....";
-	neat(); cout << endl;
-	return 0;
+terminate:
+		neat(); cout << "THANKS FOR BEING HERE " ;
+		neat(); cout << "PROCESS TERMINATING.....";
+		neat();cout << "************************************************************************" << endl;	
+		neat(); cout << endl;
+		return 0;
 	}
 	else
 	{
-		chance++;
-		if(chance == 1){
-		neat(); cout << "YOU ENTERED WRONG KEY : " << 3-chance << " chances remaining." << endl;
-		goto repeat;}
-		else if(chance == 2){
-		neat(); cout << "YOU ENTERED WRONG KEY : " << 3-chance << " chances remaining." << endl;
-		goto repeat;}
+		if(chance < 2){
+			neat(); cout << "YOU ENTERED WRONG KEY : " << 2-chance << " chances remaining." << endl;
+			chance +=1;
+			goto repeat;}
 		else {
-		neat(); cout << "SORRY YOU HAVE MADE MAXIMUM ATTEMPTS " << 3-chance << " chances remaining." << endl;
-		return 0;}
+			neat(); cout << "SORRY YOU HAVE MADE MAXIMUM ATTEMPTS " << endl;
+			goto terminate;}
 	}
-	
-	
-	
-	
-	
+
 	neat();cout << "************************************************************************" << endl;	
 }
