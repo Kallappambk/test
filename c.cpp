af88de7b07543541c1a74c47f0724f27c3e284e5
kallappa <kallappabachagundi@gmail.com>
2019-01-24 01:31:25 +0530

    test

---
+++ c.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<clocale>
+#define RED "\033[31;1,4;46m"
+#define RESET "\033[0m"
+using namespace std;
+main()
+{
+	wchar_t ch[10];
+		setlocale(LC_ALL,"");
+	
+}
+
+
---
+++ t.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	float f;
+	double d;
+	cin >> i >> f >> d ;
+	cout << i << " " << f << " " << d << endl;
+}
